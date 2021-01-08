04c6affb20e7906db483d15e23213f8784497a36
Juancarlos56 <barrerajuan930@gmail.com>
2021-01-08 15:57:35 -0500

    Creacion de Clases JUnit para test

--- ProyectoIngSoftware-CitasMedicas/src/ec/edu/ups/controladores/CertificadoMedicoControlador.java
+++ ProyectoIngSoftware-CitasMedicas/src/ec/edu/ups/controladores/CertificadoMedicoControlador.java
@@ -1,12 +1,13 @@
 package ec.edu.ups.controladores;
 
+import java.util.Calendar;
 import java.util.Date;
 
 import ec.edu.ups.entidades.Certificado;
 import ec.edu.ups.entidades.Consulta;
 
 public class CertificadoMedicoControlador {
-	public void nuevoCertificadoMedico(Date fechaEnvioDeCerficado, int numeroDiasDeReposoParaPaciente,
+	public void nuevoCertificadoMedico(Calendar fechaEnvioDeCerficado, int numeroDiasDeReposoParaPaciente,
 			String descripcionParaCertificado, Consulta certificadoDeUnaConsulta) {
 		Certificado certificado= new Certificado(fechaEnvioDeCerficado, numeroDiasDeReposoParaPaciente, descripcionParaCertificado, certificadoDeUnaConsulta);
 	}
