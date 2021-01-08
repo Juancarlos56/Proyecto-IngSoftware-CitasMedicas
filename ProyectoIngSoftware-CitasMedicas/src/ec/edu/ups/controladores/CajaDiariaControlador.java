38eac70f00d89d8b8f63bd96a7abafa3dd5b653f
gorbo96 <46572873+gorbo96@users.noreply.github.com>
2021-01-08 16:19:32 -0500

    Adicion de caja diaria controlador

---
+++ ProyectoIngSoftware-CitasMedicas/src/ec/edu/ups/controladores/CajaDiariaControlador.java
@@ -0,0 +1,12 @@
+package ec.edu.ups.controladores;
+
+import java.util.Calendar;
+
+import ec.edu.ups.entidades.CajaDiaria;
+
+public class CajaDiariaControlador {
+	public void cajaControlador(int totalDineroSalarioCajaDiaria, int totalDineroActivoCajaDiaria, int totalDineroDeudaCajaDiaria,
+			int totalDineroPatrimonioCajaDiaria, Calendar fechaCajaDiaria) {
+		CajaDiaria caja = new CajaDiaria(totalDineroSalarioCajaDiaria, totalDineroActivoCajaDiaria, totalDineroDeudaCajaDiaria, totalDineroPatrimonioCajaDiaria, fechaCajaDiaria);
+	}
+}
